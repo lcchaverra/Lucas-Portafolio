603d4c56b08687bf80c5bdeb8227d65d71030c43
lcchaverrahelpp <lcchaverra@helppeoplecloud.com>
2024-08-24 00:10:19 -0500

    update: se actualizan los datos de la experiencia laboral

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
                     <div className="hero-content text-center">
                         <div className="max-w-md">
                         <h1 className="text-5xl font-bold">Acerca de mi</h1>
-                        <p className="py-6">Desarrollador de software con experiencia en el diseño, desarrollo y mantenimiento de aplicaciones web utilizando tecnologías como React JS. He trabajado en proyectos tanto en el frontend como en el backend, utilizando frameworks como ASP.Net Core. Me destaco por mi capacidad para resolver problemas de manera eficiente y creativa, así como por mi habilidad para trabajar en equipo y colaborar en entornos diversos, contribuyendo al crecimiento y éxito de las empresas para las que he trabajado.</p>
+                        <p className="py-4">Desarrollador de software con experiencia en el diseño, desarrollo y mantenimiento de aplicaciones web utilizando tecnologías como React JS. He trabajado en proyectos tanto en el frontend como en el backend, utilizando frameworks como ASP.Net Core. Me destaco por mi capacidad para resolver problemas de manera eficiente y creativa, así como por mi habilidad para trabajar en equipo y colaborar en entornos diversos, contribuyendo al crecimiento y éxito de las empresas para las que he trabajado.</p>
                         <p className="py-2">Soy un estudiante autodidacta, con lo cual me mantengo al día con las tecnologías, he realizado Diplomados, Cursos y Bootcamps de desarrollo de software para afianzar mis conocimientos.</p>
                         {/* <p className="py-6">provengo de Quibdó – Chocó, Colombia, actualmente estoy perfeccionando mis habilidades en el desarrollo móvil.</p> */}
                         </div>
@@ -31,27 +31,37 @@ const About = () => {
                                     Junior Software Developer | HelppeopleCloud (Nov 2023 - Actualidad)
                                 </div>
                                 <div className="collapse-content"> 
-                                    <p>Desarrollé nuevos requerimientos y soportes de aplicaciones web ya existentes utilizando tecnologias como React Js y Asp.net core.</p>
+                                    <p>
+                                        Creación de requerimientos y asistencia técnica para aplicaciones web.
+                                        Uso de tecnologías avanzadas como React JS y ASP.NET Core.
+                                        Colaboración con metodologías ágiles para mejorar la eficacia y experiencia del usuario.
+                                        Trabajo en equipo con otros profesionales para soluciones innovadoras y desafíos técnicos.
+                                    </p>
                                 </div>
-                                </div>
-                                <div className="collapse collapse-plus bg-base-200">
-                                <input type="radio" id="experience-2" name="my-accordion-3" placeholder="."/> 
-                                <label htmlFor="experience-2"></label>    
-                                <div className="collapse-title text-xl font-medium">
-                                    Voluntariado | Idiomas en señas (Jul 2023 - Dic 2023)
-                                </div>
-                                <div className="collapse-content"> 
-                                    <p>Desarrollé una aplicacion con React Para la iniciativa de idiomas en señas donde desempeñe mi rol como diseñador y desarrollador fullstack.</p>
-                                </div>
-                                </div>
-                                <div className="collapse collapse-plus bg-base-200">
+                            </div>
+                            <div className="collapse collapse-plus bg-base-200">
                                 <input type="radio" id="experience-3" name="my-accordion-3" placeholder="."/> 
                                 <label htmlFor="experience-3"></label> 
                                 <div className="collapse-title text-xl font-medium">
                                     Pasante | Rentic SAS (Mar 2023- Jun 2023)
                                 </div>
                                 <div className="collapse-content"> 
-                                    <p>Desarrollé una plataforma web para el monitoreo del funcionamiento de sistemas de energía renovable sostenibles para brindarle una solución escalable a la empresa utilizando Makoserver y el lenguaje de programación Lua.</p>
+                                    <p>
+                                        Creé una plataforma en línea para supervisar sistemas de energía renovable sostenible.
+                                        Opté por Makoserver y el lenguaje Lua para desarrollar una solución escalable y efectiva.
+                                        Ayudé a mejorar la eficacia y sostenibilidad de las operaciones de la empresa con esta
+                                        herramienta innovadora.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-plus bg-base-200">
+                                <input type="radio" id="experience-2" name="my-accordion-3" placeholder="."/> 
+                                <label htmlFor="experience-2"></label>    
+                                <div className="collapse-title text-xl font-medium">
+                                    Software Developer | Facnet (Mar 2022 - Sep 2022)
+                                </div>
+                                <div className="collapse-content"> 
+                                    <p>Participación en proyectos de desarrollo de software, ocupando el Rol de Desarrollador Fullstack, En cada proyecto, pude satisfacer los requisitos del cliente y alcanzar los objetivos fijados, Hice uso de tecnologías como HTML, CSS, JavaScript, PHP, SQL, entre otras, para crear soluciones eficaces y de primera calidad.</p>
                                 </div>
                             </div>
                         </div>
